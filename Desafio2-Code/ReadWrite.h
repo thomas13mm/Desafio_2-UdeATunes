4e8ab14f3be7de31dfb1a561473caa35c1f7952e
“Thomas <t.mejia@udea.edu.co>
2025-10-21 22:12:15 -0500

    Inicializacion modulo para lectura y escritura de datos

---
+++ Desafio2-Code/ReadWrite.h
@@ -0,0 +1,28 @@
+#ifndef READWRITE_H
+#define READWRITE_H
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool readArchivo(const char* fileName) {
+    ifstream Archivo(fileName);
+    if (!Archivo.is_open()) {
+        cout << "Actualmente presentamos problemas para leer nuestros datos, lo sentimos." << '\n';
+        return false;
+    }
+    string aux;
+    while (getline(Archivo, aux)) {
+
+        cout << aux << endl;
+    }
+
+
+    Archivo.close();
+    return true;
+}
+
+
+
+#endif // READWRITE_H
