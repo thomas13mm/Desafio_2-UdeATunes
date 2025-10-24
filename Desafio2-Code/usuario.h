5278b9a1732a046c147af2cc0881aa94c3764e6d
cristianflorezm <cristian.florezm@udea.edu.co>
2025-10-24 00:18:52 -0500

    Implementa método seguirUsuario en Usuario.cpp y actualiza declaración en Usuario.h; se añade amistad con ListaRP para acceder a TamEnUso

--- Desafio2-Code/usuario.h
+++ Desafio2-Code/usuario.h
@@ -5,33 +5,42 @@
 using namespace std;
 
 //declaracion recursiva para el apuntador a tipo ListaRP
-class ListaRP {};
+class ListaRP;
 
 class Usuario {
 private:
     string nickName;
-    bool membresia;
+    bool membresia; // true = premium, false = estándar
     string pais;
     string fechaRegistro;
-    ListaRP* MiListaRP;
+    ListaRP* MiListaRP; // puntero a la lista del usuario
 
 public:
+    Usuario(); // constructor por defecto
+
+     // constructor por parametros
+    Usuario(string _nickName,
+            bool _membresia,
+            string _pais,
+            string _fechaRegistro,
+            ListaRP* _MiListaRP);
+
 
     // Getters
     string get_nickName();
     bool get_membresia();
     string get_pais();
     string get_fechaRegistro();
-    ListaRP get_MiListaRP();
+    ListaRP* get_MiListaRP();
 
     // Setters
     void set_nickName(string _nickName);
     void set_membresia(bool _membresia);
     void set_pais(string _pais);
-    void set_fechaRegistro();
+    void set_fechaRegistro(string _fechaRegistro);
     void set_MiListaRP(ListaRP* _MiListaRP);
 
-    void seguirUsuario();
+    bool seguirUsuario(Usuario& otro);
     void SiguienteCancion();
     void AnteriorCancion();
     void RepetirCancionActual();
